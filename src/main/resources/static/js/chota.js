b0e4adcedf8d665ce0a41056e7f069c0725262aa
Cliffton Fernandes <cliffton.fernandes.rulz@gmail.com>
2018-02-03 22:44:45 -0500

    - static files

---
+++ src/main/resources/static/js/chota.js
@@ -0,0 +1,45 @@
+// $('#chota-form').on('keyup keypress', function(e) {
+//     var keyCode = e.keyCode || e.which;
+//     if (keyCode === 13) {
+//         e.preventDefault();
+//         return false;
+//     }
+// });
+
+var successfunction = function (data) {
+    $("#chota-url-ref").text(data.shorturl);
+    $("#chota-url-ref").attr("href", data.shorturl);
+    $("#chota-url").show();
+    $("#chota-copy").show();
+}
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+$("#chota-copy").click(function (e) {
+    copyToClipboard(document.getElementById("chota-url-ref"));
+})
+
+
+$("#chota-submit").click(function () {
+    // alert("Chota!");
+
+    var longurl = $("#longurl").val();
+
+    $.ajax({
+        type: "POST",
+        url: "/api/link/",
+        data: JSON.stringify({ longurl: longurl }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: successfunction,
+        failure: function(errMsg) {
+            alert(errMsg);
+        }
+    });
+});
