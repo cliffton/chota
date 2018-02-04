38d69902376c5a1229d1ede3f08bbd9ebbffce8b
Cliffton Fernandes <cliffton.fernandes.rulz@gmail.com>
2018-02-03 20:09:11 -0500

    - initial commit

---
+++ src/main/java/io/cliffton/chota/ChotaApplication.java
@@ -0,0 +1,12 @@
+package io.cliffton.chota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChotaApplication.class, args);
+	}
+}
---
+++ src/test/java/io/cliffton/chota/ChotaApplicationTests.java
@@ -0,0 +1,16 @@
+package io.cliffton.chota;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChotaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
